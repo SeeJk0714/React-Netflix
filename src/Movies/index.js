c8c3662d124caa0d24324a4e3f4ebd69c9f881e6
jiekai <jiekai22s@forwardschool.edu.my>
2023-08-30 01:20:53 +0800

    u

--- src/Movies/index.js
+++ src/Movies/index.js
@@ -1,26 +1,76 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Title, Grid, Card, Badge, Group, Space } from "@mantine/core";
+import { Title, Grid, Card, Badge, Group, Space, Button } from "@mantine/core";
 
 function Movies() {
     const [movies, setMovies] = useState([]);
+    const [moviesAPI, setMoviesAPI] = useState([]);
 
     useEffect(() => {
         axios
             .get("http://localhost:5000/movies")
             .then((response) => {
                 setMovies(response.data);
+                setMoviesAPI(response.data);
             })
             .catch((error) => {
                 console.log(error);
             });
     }, []);
 
+    const filterMovie = (genre = "") => {
+        if (genre !== "") {
+            const newMovies = moviesAPI.filter((m) => m.genre === genre);
+            setMovies(newMovies);
+        } else {
+            setMovies(moviesAPI);
+        }
+    };
+
     return (
         <>
             <Title order={3} align="center">
                 Movies
             </Title>
+            <Space h="20px" />
+            <Group>
+                <Button
+                    onClick={() => {
+                        filterMovie("");
+                    }}
+                >
+                    All
+                </Button>
+                <Button
+                    onClick={() => {
+                        filterMovie("Drama");
+                    }}
+                >
+                    Drama
+                </Button>
+                <Button
+                    onClick={() => {
+                        filterMovie("Fantasy");
+                    }}
+                >
+                    Fantasy
+                </Button>
+                <Button
+                    onClick={() => {
+                        filterMovie("Action");
+                    }}
+                >
+                    Action
+                </Button>
+                <Button
+                    onClick={() => {
+                        filterMovie("Sci-Fi");
+                    }}
+                >
+                    Sci-fi
+                </Button>
+            </Group>
+            <Space h="20px" />
             <Grid>
                 {movies
                     ? movies.map((movie) => {
--- src/Tvshows/index.js
+++ src/Tvshows/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Title, Grid, Card, Badge, Group, Space } from "@mantine/core";
+import { Title, Grid, Card, Badge, Group, Space, Button } from "@mantine/core";
 
 function Tvshows() {
     const [tvshows, setTvshows] = useState([]);
@@ -16,11 +16,117 @@ function Tvshows() {
             });
     }, []);
 
+    const filterTvshow = async (genre = "") => {
+        try {
+            const response = await axios.get(
+                "http://localhost:5000/tvshows?genre=" + genre
+            );
+            setTvshows(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
         <>
             <Title order={3} align="center">
                 Tvshows
             </Title>
+            <Space h="20px" />
+            <Group>
+                <Button
+                    onClick={() => {
+                        filterTvshow("");
+                    }}
+                >
+                    All
+                </Button>
+                <Button
+                    onClick={() => {
+                        filterTvshow("Drama");
+                    }}
+                >
+                    Drama
+                </Button>
+                <Button
+                    onClick={() => {
+                        filterTvshow("Fantasy");
+                    }}
+                >
+                    Fantasy
+                </Button>
+                <Button
+                    onClick={() => {
+                        filterTvshow("Action");
+                    }}
+                >
+                    Action
+                </Button>
+                <Button
+                    onClick={() => {
+                        filterTvshow("Sci-Fi");
+                    }}
+                >
+                    Sci-fi
+                </Button>
+                <Button
+                    onClick={() => {
+                        filterTvshow("Adventure");
+                    }}
+                >
+                    Adventure
+                </Button>
+                <Button
+                    onClick={() => {
+                        filterTvshow("Comedy");
+                    }}
+                >
+                    Comedy
+                </Button>
+                <Button
+                    onClick={() => {
+                        filterTvshow("Thriller");
+                    }}
+                >
+                    Thriller
+                </Button>
+                <Button
+                    onClick={() => {
+                        filterTvshow("Horror");
+                    }}
+                >
+                    Horror
+                </Button>
+                <Button
+                    onClick={() => {
+                        filterTvshow("Crime");
+                    }}
+                >
+                    Crime
+                </Button>
+                <Button
+                    onClick={() => {
+                        filterTvshow("Mystery");
+                    }}
+                >
+                    Mystery
+                </Button>
+                <Button
+                    onClick={() => {
+                        filterTvshow("Biography");
+                    }}
+                >
+                    Biography
+                </Button>
+                <Button
+                    onClick={() => {
+                        filterTvshow("History");
+                    }}
+                >
+                    History
+                </Button>
+            </Group>
+            <Space h="20px" />
             <Grid>
                 {tvshows
                     ? tvshows.map((tvshow) => {
